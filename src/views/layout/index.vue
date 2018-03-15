3b32dafd9cfc4f43ba6dd79c18afb50682e1c26e
wulei <lei.wu@htouhui.com>
2018-03-15 18:28:25 +0800

    添加vue项目>layout导航菜单

---
+++ src/views/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <!-- 按钮区 -->
+    <layout-menu :isCollapse="collapse"></layout-menu>
+  </div>
+</template>
+
+<script>
+import layoutMenu from './components/menu/index'
+
+export default {
+  name: 'layout',
+  components: {
+    layoutMenu
+  },
+  data () {
+    return {
+      collapse: {
+        isCollapse: true
+      }
+    }
+  }
+}
+</script>
