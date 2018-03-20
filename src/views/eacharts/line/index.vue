79d56cb2dd253e8b4bd0e6ae809aef91c9de556d
wulei <lei.wu@htouhui.com>
2018-03-20 10:45:40 +0800

    折线图二bug修复

--- src/views/eacharts/line/index.vue
+++ src/views/eacharts/line/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container v-eacharts-line">
     <chart :options="polar1"></chart>
+    <chart :options="polar2"></chart>
   </div>
 </template>
 
@@ -20,7 +21,7 @@ export default {
     return {
       polar1: {
         title: {
-          text: '示例二：折线图'
+          text: '示例一：折线图'
         },
         tooltip: {},
         toolbox: {
@@ -48,13 +49,106 @@ export default {
             data: [[12, 8], [24, 10], [36, 26], [48, 30], [60, 25], [72, 20]]
           }
         ]
-      }
+      },
+      polar2: {}
     }
   },
   methods: {
     getUrl() {
-      axios.get('/gallery/data/asset/data/aqi-beijing.json').then(res => {
-        console.log(res)
+      axios.get('/api/gallery/data/asset/data/aqi-beijing.json').then(res => {
+        const arr1 = []
+        const arr2 = []
+        res.data.forEach(item => {
+          arr1.push(item[0])
+          arr2.push(item[1])
+        })
+        this.polar2 = {
+          title: {
+            text: '示例二：折线图'
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          xAxis: {
+            data: arr1
+          },
+          yAxis: {
+            splitLine: {
+              show: false
+            }
+          },
+          toolbox: {
+            left: 'center',
+            feature: {
+              dataZoom: {
+                yAxisIndex: 'none'
+              },
+              restore: {},
+              saveAsImage: {}
+            }
+          },
+          dataZoom: [{
+            startValue: '2014-06-01'
+          }, {
+            type: 'inside'
+          }],
+          visualMap: {
+            top: 10,
+            right: 10,
+            pieces: [{
+              gt: 0,
+              lte: 50,
+              color: '#096'
+            }, {
+              gt: 50,
+              lte: 100,
+              color: '#ffde33'
+            }, {
+              gt: 100,
+              lte: 150,
+              color: '#ff9933'
+            }, {
+              gt: 150,
+              lte: 200,
+              color: '#cc0033'
+            }, {
+              gt: 200,
+              lte: 300,
+              color: '#660099'
+            }, {
+              gt: 300,
+              color: '#7e0023'
+            }],
+            outOfRange: {
+              color: '#999'
+            }
+          },
+          series: {
+            name: 'Beijing AQI',
+            type: 'line',
+            data: arr2,
+            markLine: {
+              silent: true,
+              data: [
+                {
+                yAxis: 50
+                },
+                {
+                yAxis: 100
+                },
+                {
+                  yAxis: 150
+                },
+                {
+                  yAxis: 200
+                },
+                {
+                  yAxis: 300
+                }
+              ]
+            }
+          }
+        }
       })
     }
   },
