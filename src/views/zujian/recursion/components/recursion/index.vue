9eaee973a2ecbe6b15565397493c5ea68b46c2ce
wulei <lei.wu@htouhui.com>
2018-04-04 19:06:26 +0800

    递归组件测试

---
+++ src/views/zujian/recursion/components/recursion/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="v-recursion-recursion">
+    <template v-for="item in dataArr">
+      <div :key="item.id" class="txt-1">{{ item.txt }}</div>
+      <components-recursion :dataArr="item.children" v-if="item.children"></components-recursion>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'componentsRecursion',
+    props: {
+      dataArr: {
+        type: Array
+      }
+    }
+  }
+</script>
--- src/views/zujian/recursion/index.vue
+++ src/views/zujian/recursion/index.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="container v-zujian-recursion">
-    <ul>
-      <li :key="index" v-for="(item, index) in items" :class="{ 'Active': b === index }" @click="b = index">{{ item }}</li>
-    </ul>
+    <recursion :dataArr="arr"></recursion>
   </div>
 </template>
 
 <script>
+  import recursion from './components/recursion/index'
   export default {
     name: 'zujian',
+    components: {
+      recursion
+    },
+    props: {
+      arr1: {
+        type: Array
+      }
+    },
     data() {
       return {
-        items: ['11111', '22222', '33333', '44444', '55555', '66666', '77777'],
-        b: ''
+        arr: [
+          {
+            id: '1',
+            txt: 'aaa',
+            children: [
+              {
+                id: '11',
+                txt: 'aaa_aaa',
+                pid: '1'
+              },
+              {
+                id: '12',
+                txt: 'aaa_bbb',
+                pid: '1'
+              },
+              {
+                id: '13',
+                txt: 'aaa_ccc',
+                pid: '1'
+              }
+            ]
+          },
+          {
+            id: '2',
+            txt: 'bbb'
+          },
+          {
+            id: '3',
+            txt: 'ccc'
+          },
+          {
+            id: '4',
+            txt: 'ddd',
+            children: [
+              {
+                id: '41',
+                txt: 'ddd_aaa',
+                pid: '4'
+              },
+              {
+                id: '42',
+                txt: 'ddd_bbb',
+                pid: '4'
+              }
+            ]
+          },
+          {
+            id: '5',
+            txt: 'eee'
+          }
+        ]
       }
     }
   }
