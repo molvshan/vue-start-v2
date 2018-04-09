3a87bc8eb1743c39dc48021d2d6929bedbfc1ce0
wulei <lei.wu@htouhui.com>
2018-04-09 09:51:51 +0800

    递归组件优化

--- src/views/zujian/recursion/components/recursion/index.vue
+++ src/views/zujian/recursion/components/recursion/index.vue
@@ -2,6 +2,7 @@
   <div class="v-recursion-recursion">
     <template v-for="item in dataArr">
       <div :key="item.id" class="txt-1">{{ item.txt }}</div>
+      <!--eslint-disable-next-line-->
       <components-recursion :dataArr="item.children" v-if="item.children"></components-recursion>
     </template>
   </div>
