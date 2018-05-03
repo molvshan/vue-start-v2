c0ce9f3458f8b6fd33072aadc3a35e4d91295e20
wulei <lei.wu@htouhui.com>
2018-05-03 18:42:58 +0800

    vuex state部分学习内容

--- src/views/workStudy/vuex/core/state/index.vue
+++ src/views/workStudy/vuex/core/state/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="v-vuex-core-state">
-    state待开发
+    <p class="v-prism-note">单一状态树</p>
+    <p class="v-prism-txt">
+      单一状态树即用一个对象就包含了全部的应用层级状态。所以，这个对象就作为整个应用里唯一的数据源而存在。其意义就是每个应用实例仅有一个store实例。单一状态树让我们能够直接地定位任一特定的状态片段，在调试的过程中也能轻易地取得整个当前应用状态的快照。<b>单状态树和模块化并不冲突。</b>
+    </p>
   </div>
 </template>
